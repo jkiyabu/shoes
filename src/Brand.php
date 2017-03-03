9e7ad33dced5910a554db26e91d74ab5580e108d
Jayeson Kiyabu <jkiyabu@comast.net>
2017-03-02 19:16:12 -0800

    add save getAll deleteAll methods for brand model

--- src/Brand.php
+++ src/Brand.php
@@ -3,22 +3,48 @@
     {
         private $name;
         private $id;
+
         function __construct($name, $id = null)
         {
             $this->name = $name;
             $this->id = $id;
         }
+
         function setName($new_name)
         {
             $this->name = (string) $new_name;
         }
+
         function getName()
         {
             return $this->name;
         }
+
         function getId()
         {
             return $this->id;
+        }
+
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO brands (name) VALUES ('{$this->getName()}');");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+        static function getAll()
+        {
+            $brands = [];
+            $queried_brands = $GLOBALS['DB']->query("SELECT * FROM brands;");
+            foreach ($queried_brands as $brand) {
+                $id = $brand['id'];
+                $name = $brand['name'];
+                $new_brand = new Brand($name, $id);
+                array_push($brands, $new_brand);
+            }
+            return $brands;
+        }
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM brands;");
         }
     }
 ?>
--- tests/BrandTest.php
+++ tests/BrandTest.php
@@ -1,7 +1,23 @@
 <?php
+/**
+* @backupGlobals disabled
+* @backupStaticAttributes disabled
+*/
+
 require_once 'src/Brand.php';
+
+$server = 'mysql:host=localhost:3306;dbname=shoes_test';
+$username = 'root';
+$password = 'root';
+$DB = new PDO($server, $username, $password);
+
 class BrandTest extends PHPUnit_Framework_TestCase
 {
+    protected function tearDown()
+    {
+        Brand::deleteAll();
+    }
+
     function test_getId()
     {
         //Arrange
@@ -13,6 +29,7 @@ class BrandTest extends PHPUnit_Framework_TestCase
         //Assert
         $this->assertEquals(1, $result);
     }
+
     function test_getName()
     {
         //Arrange
@@ -23,6 +40,7 @@ class BrandTest extends PHPUnit_Framework_TestCase
         //Assert
         $this->assertEquals('Nike', $result);
     }
+
     function test_setName()
     {
         //Arrange
@@ -34,6 +52,48 @@ class BrandTest extends PHPUnit_Framework_TestCase
         $result = $test_Brand->getName();
         //Assert
         $this->assertEquals('Adidas', $result);
+    }
+
+function test_save()
+    {
+        //Arrange
+        $name = 'Nike';
+        $test_Brand = new Brand($name);
+        //Act
+        $test_Brand->save();
+        $result = Brand::getAll();
+        //Assert
+        $this->assertEquals([$test_Brand], $result);
+    }
+
+    function test_getAll()
+    {
+        //Arrange
+        $name1 = 'Nike';
+        $name2 = 'Adidas';
+        $test_Brand1 = new Brand($name1);
+        $test_Brand1->save();
+        $test_Brand2 = new Brand($name2);
+        $test_Brand2->save();
+        //Act
+        $result = Brand::getAll();
+        //Assert
+        $this->assertEquals([$test_Brand1, $test_Brand2], $result);
+    }
+    function test_deleteAll()
+    {
+        //Arrange
+        $name1 = 'Nike';
+        $name2 = 'Adidas';
+        $test_Brand1 = new Brand($name1);
+        $test_Brand1->save();
+        $test_Brand2 = new Brand($name2);
+        $test_Brand2->save();
+        //Act
+        Brand::deleteAll();
+        $result = Brand::getAll();
+        //Assert
+        $this->assertEquals([], $result);
     }
 }
 ?>
